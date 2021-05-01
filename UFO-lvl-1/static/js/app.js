12523dc5e251e4d525754d0e79242bc53f949004
goodman2814 <mgoodaman@gmail.com>
2021-05-01 13:09:54 -0400

    commit

--- UFO-lvl-1/static/js/app.js
+++ UFO-lvl-1/static/js/app.js
@@ -1,7 +1,12 @@
 // Get references to the tbody element, input fields and button
 let tbody = document.querySelector("tbody");
 let dateInput = document.querySelector("#datetime");
+let cityInput = document.querySelector("#city")
+let stateInput = document.querySelector("#state")
+let countryInput = document.querySelector("#country")
+let shapeInput = document.querySelector("#shape")
 let filterBtn = document.querySelector("#filter-btn");
+
 
 // from data.js
 let tableData = data;
@@ -32,13 +37,40 @@ filterBtn.addEventListener("click", handleFilterButtonClick);
 
   // Build search table for filtered data
 function handleFilterButtonClick() {
-    var filterDate = dateInput.value;
-    
+    let filterDate = dateInput.value;
+    let filterCity = cityInput.value;
+    let filterState = stateInput.value;
+    let filterCountry = countryInput.value;
+    let filterShape = shapeInput.value;
     // Filter on date
     if (filterDate != "") {
       tableData = data.filter(function (date_data) {
-        var dateDate = address.datetime;
+        let dateDate = date_data.datetime;
         return dateDate === filterDate;
+      });
+    }
+    if (filterCity != "") {
+      tableData = data.filter(function (city_data) {
+        let cityCity = city_data.city;
+        return cityCity === filterCity;
+      });
+    }
+    if (filterState != "") {
+      tableData = data.filter(function (state_data) {
+        let stateState = state_data.state;
+        return stateState === filterState;
+      });
+    }
+    if (filterCountry != "") {
+      tableData = data.filter(function (country_data) {
+        let countryCountry = country_data.country;
+        return countryCountry === filterCountry;
+      });
+    }
+    if (filterShape != "") {
+      tableData = data.filter(function (shape_data) {
+        let shapeShape = shape_data.shape;
+        return shapeShape === filterShape;
       });
     }
     else { tableData };
